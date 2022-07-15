9f19343e0810edb84c61232fc73fda4905d2ede3
TukeBean <tukesimpson@gmail.com>
2022-07-16 00:04:30 +0100

    cube dad updaterz

--- Assets/CubeController.cs
+++ Assets/CubeController.cs
@@ -5,20 +5,10 @@ using UnityEngine;
 public class CubeController : MonoBehaviour
 {
     public int torqueLit = 100;
-    public float mSpeed = 2f;
-    Vector3 myVectorForward;
-    Vector3 myVectorBackward;
-    Vector3 myVectorLeft;
-    Vector3 myVectorRight;
-    
     Rigidbody mRigidBody;
     // Start is called before the first frame update
     void Start()
     {
-        myVectorForward = new Vector3(0.0f, 0.0f, 1.0f);
-        myVectorBackward = new Vector3(0.0f, 0.0f, -1.0f);
-        myVectorLeft = new Vector3(0.0f, 0.0f, 0.0f);
-        myVectorRight = new Vector3(0.0f, 0.0f, 0.0f);
         mRigidBody = GetComponent<Rigidbody>();
     }
 
@@ -33,22 +23,19 @@ public class CubeController : MonoBehaviour
         if (Input.GetKey(KeyCode.W)) 
         { 
             mRigidBody.AddTorque(torqueLit, 100, 0);
-            mRigidBody.velocity = myVectorForward * mSpeed;
+            
         }
         if (Input.GetKey(KeyCode.A)) 
         { 
             mRigidBody.AddTorque(0, 100, torqueLit);
-            mRigidBody.velocity = myVectorLeft * mSpeed;
         }
         if (Input.GetKey(KeyCode.S)) 
         { 
             mRigidBody.AddTorque(-torqueLit, 100, 0);
-            mRigidBody.velocity = myVectorBackward * mSpeed;
         }
         if (Input.GetKey(KeyCode.D)) 
         { 
             mRigidBody.AddTorque(0, 100, -torqueLit);
-            mRigidBody.velocity = myVectorRight * mSpeed;
         }
     }
 }
